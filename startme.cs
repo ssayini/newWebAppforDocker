7598ba1d2648faadcb43f4cfeb53c80e39c858ea
Srinivas Sayini <ssayini@hotmail.com>
2020-11-15 14:39:31 -0500

    Add project files.

---
+++ startme.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newWebAppforDocker
+{
+    public class startme
+    {
+        public startme(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+    }
+}
