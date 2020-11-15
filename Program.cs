1fd188dd2a910df5428fd52f9313e1e836850bc2
Srinivas Sayini <ssayini@hotmail.com>
2020-11-15 14:41:21 -0500

    no changes

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace newWebAppforDocker
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                { 
                     webBuilder.UseStartup<Startup>();
                 });
     }
